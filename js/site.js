def5398d4c4a13ace04ce63d9fbbe0086c418d42
Aury Bwashi <abwashi@hawk.iit.edu>
2016-12-01 23:05:07 -0600

    Country's information gets updated now instead of being appended

--- js/site.js
+++ js/site.js
@@ -3,7 +3,17 @@ $(document).ready(function() {
 
     $('#gh-form').on('submit', function(event) {
         event.preventDefault();
+
+        //this checks to see if a previous country's information is already displayed.
+        //if some information is displayed, it clears it out the element for the next search
+        if (!$('#primary').is(':empty')) {
+            $('#primary').empty();
+        }
+
+        // get the value of the string entered by user into variable called "place"
         let place = document.getElementById('gh-location').value;
+
+        //get the geocoded value of place and use it to make a map with initMap function
         let geocoder = new google.maps.Geocoder();
         geocoder.geocode({
             'address': place
@@ -13,18 +23,19 @@ $(document).ready(function() {
         countryInfo(place);
     });
 
+    //get country information from RESTcountries API.
     function countryInfo(location) {
         $.get(
             'https://restcountries.eu/rest/v1/name/' + location,
             function(data) {
-                console.log(data[0]);
-                var name = data[0].name;
-                var capital = data[0].capital;
-                var twoLetterCountryCode = data[0].alpha2Code;
-                var threeLetterCountryCode = data[0].alpha3Code;
-                var callingCode = data[0].callingCodes[0];
-                var region = data[0].region;
-                var subregion = data[0].subregion;
+                var c = data[0];
+                var name = c.name;
+                var capital = c.capital;
+                var twoLetterCountryCode = c.alpha2Code;
+                var threeLetterCountryCode = c.alpha3Code;
+                var callingCode = c.callingCodes[0];
+                var region = c.region;
+                var subregion = c.subregion;
 
                 var list = '<ul id="country-details">' +
                     '<li class="info-label" id="country-name">Country</li>' +
@@ -38,7 +49,7 @@ $(document).ready(function() {
                     '<li class="info-label" id="calling-code">Calling code</li>' +
                     '<li class="info">' + callingCode + '</li>' +
                     '<li class="info-label" id="country-region">Region</li>' +
-                    '<li class="info">' + region  + '</li>' +
+                    '<li class="info">' + region + '</li>' +
                     '<li class="info-label" id="subregion">Subregion</li>' +
                     '<li class="info">' + subregion + '</li>' +
                     '</ul>';
